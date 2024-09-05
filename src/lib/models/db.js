5096fb2733c748cbc056a1e841df3a6bc72d5b2e
cocoro0611 <chiroru.0903.0611@icloud.com>
2024-09-05 19:01:35 +0900

    DB連携

---
+++ src/lib/models/db.js
@@ -0,0 +1,27 @@
+import { DB_HOST, DB_PORT, DB_NAME, DB_USER, DB_PASSWORD } from '$env/static/private';
+import { Kysely, PostgresDialect } from 'kysely';
+import pg from 'pg';
+const { Pool } = pg;
+
+// .envファイルはこれを使う必要あり
+import dotenv from 'dotenv';
+dotenv.config();
+
+const pool = new Pool({
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    database: process.env.DB_NAME,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    ssl: false,
+});
+
+const dialect = new PostgresDialect({
+    pool,
+});
+
+const db = new Kysely({
+    dialect,
+});
+
+export default db;
