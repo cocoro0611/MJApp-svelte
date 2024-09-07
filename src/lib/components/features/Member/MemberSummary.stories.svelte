47178553b7dfedea0ef9869b50579c81f563679b
cocoro0611 <chiroru.0903.0611@icloud.com>
2024-09-07 20:10:56 +0900

    DB

---
+++ src/lib/components/features/Member/MemberSummary.stories.svelte
@@ -0,0 +1,18 @@
+<script lang="ts" context="module">
+  import type { Meta } from "@storybook/svelte";
+
+  import MemberSummary from "./MemberSummary.svelte";
+
+  export const meta: Meta<MemberSummary> = {
+    title: "features/Point-Fusuu/MemberSummary",
+    component: MemberSummary,
+  };
+</script>
+
+<script lang="ts">
+  import { Story } from "@storybook/addon-svelte-csf";
+</script>
+
+<Story name="Default">
+  <MemberSummary />
+</Story>
