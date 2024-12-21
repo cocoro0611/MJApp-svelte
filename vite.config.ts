669db197e09d1fb950b42387c3d85dded3569ce2
Shinta Takagishi <chiroru.0903.0611@icloud.com>
2024-12-22 01:06:39 +0900

    ローカルで

--- vite.config.ts
+++ vite.config.ts
@@ -3,4 +3,8 @@ import { defineConfig } from "vite";
 
 export default defineConfig({
   plugins: [sveltekit()],
+  server: {
+    host: "0.0.0.0", // すべてのネットワークインターフェースでリッスン
+    port: 5173, // ポートは必要に応じて変更可能
+  },
 });
