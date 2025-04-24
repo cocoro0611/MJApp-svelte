ff3f7180e3a07a106b0f8561bee933ab21f7c2b7
Shinta Takagishi <chiroru.0903.0611@icloud.com>
2025-04-24 13:19:07 +0900

    aaa

--- src/lib/models/db.js
+++ src/lib/models/db.js
@@ -1,3 +1,10 @@
+import {
+  DB_HOST,
+  DB_PORT,
+  DB_NAME,
+  DB_USER,
+  DB_PASSWORD,
+} from "$env/static/private";
 import { Kysely, PostgresDialect } from "kysely";
 import pg from "pg";
 const { Pool } = pg;
@@ -7,7 +14,12 @@ import dotenv from "dotenv";
 dotenv.config();
 
 const pool = new Pool({
-  connectionString: process.env.DATABASE_URL,
+  host: DB_HOST,
+  port: Number(DB_PORT),
+  database: DB_NAME,
+  user: DB_USER,
+  password: DB_PASSWORD,
+  ssl: false,
 });
 
 const dialect = new PostgresDialect({
